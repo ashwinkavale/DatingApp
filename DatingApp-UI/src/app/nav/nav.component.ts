9bb71f3319d1c875ddf89be19dfe0e5a11477f52
Ashwin Kavale <ashwinkavale@gmail.com>
2019-11-20 02:02:35 +0530

    register form

--- DatingApp-UI/src/app/nav/nav.component.ts
+++ DatingApp-UI/src/app/nav/nav.component.ts
@@ -22,4 +22,15 @@ export class NavComponent implements OnInit {
     });
   }
 
+  loggedIn()
+  {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout()
+  {
+    localStorage.removeItem('token');
+  }
+
 }
